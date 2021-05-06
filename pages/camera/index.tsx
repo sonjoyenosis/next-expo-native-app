edd8d9356ffceaf56a98dda70d2c09cd3a581fdd
Sonjoy Dabnath <sonjoydabnath009@gmail.com>
2021-05-06 14:48:08 +0600

    Create a basic camera test UI to display captured images with exif data, camera ratio-resolution info

--- pages/camera/index.tsx
+++ pages/camera/index.tsx
@@ -6,6 +6,10 @@ export default function CameraTest() {
   const [hasPermission, setHasPermission] = useState<boolean>();
   const [type, setType] = useState(Camera.Constants.Type.back);
 
+  const [cameraReady, setCameraReady] = useState(false);
+  const [cameraResolutionInfo, setCameraResolutionInfo] = useState<string[]>(
+    []
+  );
   const [images, setImages] = useState<CameraCapturedPicture[]>([]);
 
   let camRef = React.useRef<Camera | null>(null);
@@ -16,6 +20,56 @@ export default function CameraTest() {
       setHasPermission(status === "granted");
     })();
   }, []);
+
+  const onCameraReady = () => {
+    const ratios = ["1:1", "4:3", "16.9"];
+
+    setCameraReady(true);
+
+    Promise.all(
+      ratios.map((r) => {
+        return camRef.current
+          ?.getAvailablePictureSizesAsync(r)
+          .then((picSize) => {
+            const camResInfo = `ratio: ${r}, size: ${picSize}\n`;
+            return camResInfo;
+          })
+          .catch((err) => {
+            console.log(
+              "Err getting the camera resolution info, er =",
+              err.message
+            );
+            return "";
+          });
+      })
+    ).then((result) => {
+      setCameraResolutionInfo(result as string[]);
+    });
+  };
+
+  const handleCapture = async () => {
+    if (!cameraReady) {
+      console.log("Camera is not ready yet!");
+      return;
+    }
+
+    if (camRef.current) {
+      console.log("camera found! taking picture....");
+
+      const capturedPhoto = await camRef.current.takePictureAsync({
+        imageType: ImageType.png,
+        quality: 1.0,
+        scale: 1,
+
+        skipProcessing: true,
+      });
+
+      console.log(capturedPhoto);
+      setImages([...images, capturedPhoto]);
+    } else {
+      console.log("Camera ref is not found");
+    }
+  };
 
   if (hasPermission === null) {
     return <h1> Waiting For Camera Permissions </h1>;
@@ -33,10 +87,18 @@ export default function CameraTest() {
         pictureSize={"1920x1080"}
         autoFocus={"on"}
         focusable
+        onCameraReady={onCameraReady}
         ref={(ref) => {
           camRef.current = ref;
         }}
       />
+
+      <div>
+        {cameraResolutionInfo.map((camRes) => (
+          <p> {camRes} </p>
+        ))}
+      </div>
+
       <div>
         <button
           style={styles.button}
@@ -51,51 +113,39 @@ export default function CameraTest() {
           <span style={styles.text}> Flip </span>
         </button>
 
-        <button
-          style={styles.button}
-          onClick={async () => {
-            if (camRef.current) {
-              console.log("camera found! taking picture....");
-
-              const ratios = ["1:1", "4:3", "16.9"];
-              console.log(ratios);
-
-              await ratios.map(async (r) => {
-                const picSize = await camRef.current?.getAvailablePictureSizesAsync(
-                  r
-                );
-                console.log("ratio = ", r, "size =", picSize);
-              });
-
-              const capturedPhoto = await camRef.current.takePictureAsync({
-                imageType: ImageType.png,
-                quality: 1.0,
-                scale: 1,
-
-                skipProcessing: true,
-              });
-
-              console.log({ capturedPhoto });
-              setImages([...images, capturedPhoto]);
-              console.log("images =", images);
-            } else {
-              console.log("Camera ref is not found");
-              alert("Camera ref is not found");
-            }
-          }}
-        >
+        <button style={styles.button} onClick={handleCapture}>
           <span style={styles.text}> Capture </span>
         </button>
       </div>
+
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
         {images.map((image, idx) => {
-          console.log("image index =", idx);
           return (
-            <img
-              src={image.uri}
-              height={image.height / 5}
-              width={image.width / 5}
-            />
+            <div
+              style={{
+                height: `${image.height / 3 + 120}px`,
+                width: `${image.width / 3}px`,
+              }}
+            >
+              <img
+                src={image.uri}
+                height={image.height / 3}
+                width={image.width / 3}
+              />
+              <p
+                style={{
+                  height: `${100}px`,
+                  width: `${image.width / 3}px`,
+                  overflowY: "visible",
+                  overflowX: "scroll",
+                }}
+              >
+                aspectRatio: {Number(image.exif.aspectRatio).toFixed(3)} <br />
+                height: {image.exif.height} <br />
+                width: {image.exif.width} <br />
+                resizeMode: {image.exif.resizeMode} <br />
+              </p>
+            </div>
           );
         })}
       </div>
